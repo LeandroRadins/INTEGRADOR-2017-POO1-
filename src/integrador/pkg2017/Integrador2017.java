4100958bf047687f21444bd947ae94e25fbdb10e
Leandro Radins <radins97@gmail.com>
2017-10-05 14:37:25 -0300

    segundo commit

--- src/integrador/pkg2017/Integrador2017.java
+++ src/integrador/pkg2017/Integrador2017.java
@@ -4,6 +4,6 @@ package integrador.pkg2017;
 /*@author Radins Leandro-Rodriguez Fernando.*/
 public class Integrador2017 {
     public static void main(String[] args) {
+        System.err.println("hola mundo");
     }
-    
 }
